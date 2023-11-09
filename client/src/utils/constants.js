2c891395db14eb6d9853af696abf80f1120a5908
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2023-11-09 16:14:31 +0100

    il tuo messaggio di commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x4EB81C8Ec65F47E185DB712b901675361D4d567F";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/SYYCzkYmrIkVSKP1SLnbLYDB8Wk4NNUI',
+      accounts: ['0a6581963cb57182f6974ac52c5ce88e7e09a57ac295ce5efcaa36ce1b12af39'],
+    },
+  },
+};//0x4EB81C8Ec65F47E185DB712b901675361D4d567F
